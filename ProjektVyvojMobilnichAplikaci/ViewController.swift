c091f10f0924017f9badea72ec3bad3382c879f0
lukaslizal <luky.lizal@gmail.com>
2019-03-18 10:22:44 +0100

    Initial Commit

---
+++ ProjektVyvojMobilnichAplikaci/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjektVyvojMobilnichAplikaci
+//
+//  Created by Lukas on 18/03/2019.
+//  Copyright © 2019 Lukas and Filip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
